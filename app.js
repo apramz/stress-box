b92f1c9e8fe48648ca6dd0d301cf5bac50573d35
apramz <apramono@berkeley.edu>
2016-05-09 21:28:34 -0700

    whole bunch of changes
    made it so table works, can delete on spot, added about pages

--- app.js
+++ app.js
@@ -1,34 +1,5 @@
 var main = function() {
 
-/*****Adding Custom Name Function*****/
-    
-    /*--WINDOW PROMPT FOR NAME--
-    if($('.home').length > 0){
-        var name=prompt("Hi there!\nWhat's your name?");
-        if(name!= null){
-            $('h1 a').text(name);
-        }
-        else {
-            $('h1 a').text('there')
-        }
-    }
-    */
-
-    /*--CODE FOR BLANK HOME PAGE NAME PROMPT--
-    $('#input1').keyup(function (e){
-
-        if (e.keyCode == 13){
-            var name = document.getElementById("input1").value;
-            $('#input1').remove();
-            document.getElementById("greeting").innerHTML = "Hey " + name + ". What's bothering you?";
-
-            $('#start').css('visibility', 'visible');  
-            $('#blurb').css('visibility', 'visible');          
-        }
-
-    });
-
-    */
 
 /****CODE FOR CUSTOM NAME PAGE PROMPT****/
      $('#input1').keyup(function (e){
@@ -37,22 +8,23 @@ var main = function() {
             var name = document.getElementById("input1").value;
             $('#input1').remove();
             $('#prompt').remove();
-            document.getElementById("greeting").innerHTML = "Hi " + name + ". What's bothering you?";      
+            document.getElementById("greeting").innerHTML = "Hi " + name + ". What's bothering you?";
+            $('#start').css('visibility', 'visible').fadeIn(1000);  
+            $('#blurb').css('visibility', 'visible').fadeIn(1000)      
         }
 
     });
 
 /*****List Posting Box Function*****/
-    
+    /*
     $('#post').click(function() {
         var post = $('.status-box').val();
-        /*$('<li>').text(post).prependTo('.posts');*/
         var item = $('<li>').text(post);
         $('.posts').prepend(item);
         $('.status-box').val('');
         $('.counter').text('140');
         $('#post').addClass('disabled');
-    });
+    });*/
 
     $('.status-box').keyup(function() {
     	var postLength = $(this).val().length;
@@ -73,7 +45,7 @@ var main = function() {
     $('#post').addClass('disabled');    
 
 /*****List Click Function*****/
-
+/*
     $('.posts').on('click', 'li', function() {
 		if($(this).hasClass('selected')) {
             $(this).removeClass('selected pulse').effect('shake', {times: 1}, 300);
@@ -86,34 +58,35 @@ var main = function() {
     $('#delete').click(function() {
     	$('.selected').remove();
 	});
-
+*/
 /*****Table Posting Box Function****/
 
     $('#post').click(function(){
         var post = $('.status-box').val();
-        var
-        $('#posts tbody').append(
-            "<tr>"+
-            "<td>"+
-            post+
-            "</td>"+
-            "<td><a href='#'>blah</a></td>"+
-            "</tr>"
-            );              
+        $('#posts tbody').prepend(
+            "<tr><td class='entry'>"+post+"</td><td id='delete' style='cursor:hand'><a href='#'>X</a></td></tr>");
+        $('.status-box').val('');
+        $('.counter').text('140');
+        $('#post').addClass('disabled');                  
     });
 
+    $('.status-box').keyup(function (e){
+        if (e.keyCode == 13){    
+            var post = $('.status-box').val();
+            $('#posts tbody').prepend(
+                "<tr><td class='entry'>"+post+"</td><td id='delete'><a href='#'>X</a></td></tr>");
+            $('.status-box').val('');
+            $('.counter').text('140');
+            $('#post').addClass('disabled');  
+        }                
+    });            
 
-/*****Believe Button Function*****
+/*****Table Delete Function****/
 
-    $('#believe').dialog({
-        autoOpen: false,
-        minHeight: 100,
-    });    
-
-    $('#opener').click(function() {
-        $('#believe').dialog('open').fadeIn(500);
+    $('#posts').on("click", "#delete", function() {
+        $(this).parent('tr:first').remove();
     });
-*/
+
 /*****Pulse Button Function*****/
 
     $('#start').click(function(){
